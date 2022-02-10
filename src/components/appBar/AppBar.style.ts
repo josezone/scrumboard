19e21afa800035405868b3b0898bdd18b10a6f58
Sofia Shajahan <sofia@qburst.com>
2022-02-10 22:05:00 +0530

    add project and sprint selection inside app bar.

---
+++ src/components/appBar/AppBar.style.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const AppBarWrapper = styled.div`
+  .MuiToolbar-root {
+    padding-left: 0;
+    padding-right: 0;
+    background-color: #ffff;
+    
+  }
+
+  .MuiContainer-root {
+    padding-left: 0px;
+    padding-right: 0px;
+  }
+
+  .MuiAppBar-root{
+      background-color: #ffff;
+      box-shadow: none;
+      border-bottom: 2px solid #1976d2;
+
+  }
+`;
--- src/components/appBar/AppBar.tsx
+++ src/components/appBar/AppBar.tsx
@@ -5,8 +5,11 @@ import Container from "@mui/material/Container";
 
 import ItemSelect from "../itemSelect/ItemSelect";
 
+import { AppBarWrapper } from "./AppBar.style";
+
 type yearItem = { key: number; value: number; label: number };
 type scrumItem = { key: number; value: number; label: number };
+type sprintItem = { key: number; value: number; label: string };
 
 function AppBarHeader(props: any) {
   const year = new Date().getFullYear();
@@ -28,6 +31,23 @@ function AppBarHeader(props: any) {
       label: date,
     };
   });
+  const sprintItems: Array<sprintItem> = props.sprintList?.map(
+    (sprint: any) => {
+      return {
+        key: sprint?.id,
+        value: sprint?.id,
+        label: sprint.sprint,
+      };
+    }
+  );
+
+  const projectItems: Array<any> = props.projectList?.map((project: any) => {
+    return {
+      key: project.id,
+      value: project?.id,
+      label: project.project,
+    };
+  });
 
   const onYearChange = (event: any) => {
     props.send({ type: "yearChanged", prop: event.target.value });
@@ -44,36 +64,77 @@ function AppBarHeader(props: any) {
     props.send({ type: "scrumChanged", prop: selectedObj });
   };
 
+  const onSprintChange = (event: any) => {
+    const selectedSprintNew = props?.sprintList?.filter(
+      (sprint: any) => sprint.id === Number(event.target.value)
+    );
+    if (selectedSprintNew.length) {
+      props.send({ type: "sprintChanged", prop: selectedSprintNew[0] });
+    }
+  };
+
+  const onProjectChanged = (event: any) => {
+    const project = props?.projectList?.filter(
+      (project: any) => project.id === Number(event.target.value)
+    );
+    if (project.length) {
+      props.send({ type: "projectChanged", prop: project[0] });
+    }
+  };
   return (
-    <AppBar position="static">
-      <Container maxWidth={false}>
-        <Toolbar>
-          <Box sx={{ flexGrow: 1, display: "flex" }}>
-            {props.selectedScrum &&
-              props.scrumList &&
-              props.scrumList.length && (
+    <AppBarWrapper>
+      <AppBar position="static">
+        <Container maxWidth={false}>
+          <Toolbar>
+            <Box sx={{ flexGrow: 1, display: "flex" }}>
+              {props.selectedScrum &&
+                props.scrumList &&
+                props.scrumList.length && (
+                  <ItemSelect
+                    items={scrumItems}
+                    defaultItem={props.selectedScrum.id}
+                    onChange={onScrumChange}
+                    name="Scrum"
+                    id="selectScrumNative"
+                  />
+                )}
+
+              {props.sprintList &&
+                props.sprintList.length &&
+                props.selectedSprint && (
+                  <ItemSelect
+                    items={sprintItems}
+                    defaultItem={props.selectedSprint.id}
+                    onChange={onSprintChange}
+                    name="Sprint"
+                    id="selectSprintNative"
+                  />
+                )}
+
+              {props.selectedProject && (
                 <ItemSelect
-                  items={scrumItems}
-                  defaultItem={props.selectedScrum.id}
-                  onChange={onScrumChange}
-                  name="Scrum"
-                  id="selectNative"
+                  items={projectItems}
+                  defaultItem={props.selectedProject.id}
+                  onChange={onProjectChanged}
+                  name="Project"
+                  id="selectProjectNative"
                 />
               )}
-          </Box>
+            </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <ItemSelect
-              items={yearItems}
-              defaultItem={year.toString()}
-              onChange={onYearChange}
-              name="Year"
-              id="selectYearNative"
-            />
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            <Box sx={{ flexGrow: 0 }}>
+              <ItemSelect
+                items={yearItems}
+                defaultItem={year.toString()}
+                onChange={onYearChange}
+                name="Year"
+                id="selectYearNative"
+              />
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </AppBarWrapper>
   );
 }
 export default AppBarHeader;
