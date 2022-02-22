d11376761767df7665b049b5b95f56915d7bd033
maddev.jose <joset@yeswearemad.com>
2022-02-22 07:40:26 +0530

    update estimate machine

--- src/pages/Estimate/estimateMachine.ts
+++ src/pages/Estimate/estimateMachine.ts
@@ -1,60 +1,60 @@
 import { createMachine } from "xstate";
 
 export const estimateMachine = createMachine<any>({
-    id: "main",
-    type: "parallel",
-    initial: "home",
-    context: {
-        ticketList: undefined,
-        selectedTicketId: undefined,
-    },
-    on: {
-        removeEstimate: {
-            target: "removeEstimate",
-        },
-    },
-    states: {
-        home: {
-            id: "home",
-            initial: "getTicketList",
-            states: {
-                getTicketList: {
-                    invoke: {
-                        id: "getTicketList",
-                        src: "invokeGetTicketList",
-                        onDone: {
-                            actions: "assignTicketList",
-                            target: "end",
-                        },
-                        onError: {
-                            target: "end",
-                        },
-                    },
-                },
-                end: {
-                    type: "final",
-                },
+  id: "main",
+  type: "parallel",
+  initial: "home",
+  context: {
+    ticketList: undefined,
+    selectedTicketId: undefined,
+  },
+  states: {
+    home: {
+      id: "home",
+      initial: "getTicketList",
+      states: {
+        getTicketList: {
+          invoke: {
+            id: "getTicketList",
+            src: "invokeGetTicketList",
+            onDone: {
+              actions: "assignTicketList",
+              target: "end",
             },
-        },
-        removeEstimate: {
-            states: {
-                removeEstimate: {
-                    invoke: {
-                        id: "removeTicket",
-                        src: "invokeRemoveTicket",
-                        onDone: {
-                            actions: "removeTicketAction",
-                            target: "end",
-                        },
-                        onError: {
-                            target: "end",
-                        },
-                    },
-                },
-                end: {
-                    type: "final",
-                },
+            onError: {
+              target: "end",
             },
+          },
         },
+        end: {
+          type: "final",
+        },
+      },
     },
+    removeEstimate: {
+      initial: "idle",
+      states: {
+        idle: {
+          on: {
+            removeEstimate: {
+              target: "removeEstimateStart",
+            },
+          },
+        },
+        removeEstimateStart: {
+          invoke: {
+            id: "removeTicket",
+            src: "invokeRemoveTicket",
+            onDone: {
+              actions: "removeTicketAction",
+              target: "idle",
+            },
+            onError: {
+              target: "idle",
+            },
+          },
+        },
+      },
+    },
+  },
 });
