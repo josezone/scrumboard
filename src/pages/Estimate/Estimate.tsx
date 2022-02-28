47d5d22ad6b4ff197aab34acf85e8462ea8d8901
maddev.jose <joset@yeswearemad.com>
2022-02-28 19:50:10 +0530

    remove auth

--- src/pages/Estimate/Estimate.tsx
+++ src/pages/Estimate/Estimate.tsx
@@ -5,10 +5,10 @@ import { estimateMachine } from "./estimateMachine";
 import { actions } from "./stateActions";
 
 function Estimate(props: any) {
-    props.graphQLClient.setHeader(
-        "Authorization",
-        "Basic " + localStorage.getItem("data")
-    );
+    // props.graphQLClient.setHeader(
+    //     "Authorization",
+    //     "Basic " + localStorage.getItem("data")
+    // );
 
     const services = useServices(props);
     const [state, send] = useMachine(estimateMachine, { actions, services });
