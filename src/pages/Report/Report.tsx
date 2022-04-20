c5bc8e2ba0dc6724ec770bfec6838cbb169fce51
maddev.jose <joset@yeswearemad.com>
2022-04-20 15:33:19 +0530

    update report

--- src/pages/Report/Report.tsx
+++ src/pages/Report/Report.tsx
@@ -9,11 +9,6 @@ import Tabs from "@mui/material/Tabs";
 
 function TabPanel(props: any) {
 
-  props.graphQLClient.setHeader(
-    "Authorization",
-    "Basic " + localStorage.getItem("data")
-  );
-
   const { children, value, index, ...other } = props;
 
   return (
@@ -30,6 +25,12 @@ function TabPanel(props: any) {
 }
 
 function Report(props: any) {
+  
+  props.graphQLClient.setHeader(
+    "Authorization",
+    "Basic " + localStorage.getItem("data")
+  );
+
   const [value, setValue] = useState(0);
 
   const handleChange = (event: any, newValue: any) => {
